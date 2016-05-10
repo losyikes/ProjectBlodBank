0c716ea572df44c71b6184afd97eb5083736307b
Daniel Alexander Nedergaard <DanielxNedergaard@hotmail.dk>
2016-05-10 09:35:03 +0200

    Revert "Added data connection with the Data Sources window"
    This reverts commit 61c9129b6a70df22e3eaa3a2d7f4084c1b2a13d8.

--- ProjektBlodbank/Properties/Settings.Designer.cs
+++ ProjektBlodbank/Properties/Settings.Designer.cs
@@ -33,16 +33,5 @@ namespace ProjektBlodbank.Properties {
                 return ((string)(this["DatabaseConnectionString"]));
             }
         }
-        
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\Database1.mdf" +
-            ";Integrated Security=True")]
-        public string Database1ConnectionString {
-            get {
-                return ((string)(this["Database1ConnectionString"]));
-            }
-        }
     }
 }
