1142803f52bf6cf79b092595effa1ae1fa01263d
Daniel Alexander Nedergaard <DanielxNedergaard@hotmail.dk>
2016-05-10 11:58:59 +0200

    Added UserData.cs and linq to SQL

---
+++ ProjektBlodbank/Data/UserData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Linq;
+
+namespace ProjektBlodbank.Data
+{
+    class UserData
+    {
+        private DataContext db;
+
+        public UserData()
+        {
+            db = new DataContext("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\Database.mdf;Integrated Security=True");
+        }
+
+        /*
+        Uncomment when User class is added
+
+        public User GetUser(int userID)
+        {
+            var user = from x in db.GetTable<User>()
+                     where x.userID == userID
+                     select x;
+            return user;
+        }
+        */
+    }
+}
