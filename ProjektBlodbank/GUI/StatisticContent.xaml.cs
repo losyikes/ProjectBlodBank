e9e57813450745378a0043957d509e33426e41e6
Daniel Grønning <curac.dg@gmail.com>
2016-05-18 13:22:54 +0200

    Added some GUI and small function to showing statistics

--- ProjektBlodbank/GUI/StatisticContent.xaml.cs
+++ ProjektBlodbank/GUI/StatisticContent.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,21 +22,37 @@ namespace ProjektBlodbank.GUI
     
     public partial class StatisticContent : UserControl
     {
-        Controller chart = new Controller();
+        Controller statController = new Controller();
+
+        
         public StatisticContent()
         {
             InitializeComponent();
-            
-            
-            
-            
+
+            statController.
+
+
+
+
+
+
             //chart.statContent = this;
-
-
-
-
             //Grid statGrid = chart.CreateGrid();
             //StatisticsContentGrid.Children.Add(statGrid);
         }
     }
 }
+
+
+
+//            <Label x:Name="lblMemberDurationStat" Content="" HorizontalAlignment="Left" Margin="203,0,0,0" VerticalAlignment="Top" Width="155"/>
+//            <Label x:Name="lblTotalDonationStat" Content="" HorizontalAlignment="Left" Margin="203,26,0,0" VerticalAlignment="Top" Width="155"/>
+//            <Label x:Name="lblTotDonWholeStat" Content="" HorizontalAlignment="Left" Margin="203,52,0,0" VerticalAlignment="Top" Width="155"/>
+//            <Label x:Name="lblTotDonPlasmaStat" Content="" HorizontalAlignment="Left" Margin="203,78,0,0" VerticalAlignment="Top" Width="155"/>
+//            <Label x:Name="lblAmountDonatedStat" Content="" HorizontalAlignment="Left" Margin="203,104,0,0" VerticalAlignment="Top" Width="155"/>
+//            <Label x:Name="lblAmDonWholeStat" Content="" HorizontalAlignment="Left" Margin="203,130,0,0" VerticalAlignment="Top" Width="155"/>
+//            <Label x:Name="lblAmDonPlasmaStat" Content="" HorizontalAlignment="Left" Margin="203,156,0,0" VerticalAlignment="Top" Width="155"/>
+//            <Label x:Name="lblTotalTimeDonatedStat" Content="" HorizontalAlignment="Left" Margin="203,182,0,0" VerticalAlignment="Top" Width="155"/>
+//            <Label x:Name="lblTotTimeWholeStat" Content="" HorizontalAlignment="Left" Margin="203,208,0,0" VerticalAlignment="Top" Width="155"/>
+//            <Label x:Name="lblTotTimePlasmaStat" Content="Label" HorizontalAlignment="Left" Margin="203,234,0,0" VerticalAlignment="Top" Width="155"/>
+//            <Label x:Name="lblEmptyStat" Content="" HorizontalAlignment="Left" Margin="203,260,0,0" VerticalAlignment="Top" Width="155"/>
